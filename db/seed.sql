4b685d4bf9bcc55e3d40abd26a4ae08f945197d3
Markedia Hinds <mhinds@pursuit.org>
2024-07-21 13:42:53 -0400

    Data update

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,6 @@
 INSERT INTO herbs (
     name, entry_date, nutrients, astrology_sign, chakra, element, tea, poisonous, stock
 ) VALUES 
-('Basil','6-12-2020', 'Vitamins C, Calcium, Iron, Vitamin D, Magnesium','Pisces, Aries','Solar Plexus','Fire', true, false, 10);
+('Basil','6-12-2020', 'Vitamin C, Calcium, Iron, Vitamin D, Magnesium','Pisces, Aries','Solar Plexus','Fire', true, false, 10)
+('Hyssop','4-3-2020', 'Vitamin A, Calcium, Iron, Vitamin C, Tannins','Cancer','Throat','Fire', true, false, 3);
+('Arnica','8-28-2020', 'Vitamin C, Calcium, Sesquiterpene, Flavonoids, Helenalin','Leo, Aries, Aquarius','Solar Plexus','Fire', true, true, 0);
